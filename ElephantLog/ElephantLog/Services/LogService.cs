cad413a91148dda5d9068010a7a7d5667ea9e3a1
Hugo van Rijswijk <hugo.v.rijswijk@gmail.com>
2017-11-14 14:59:25 +0100

    Use logger in LogService

--- ElephantLog/ElephantLog/Services/LogService.cs
+++ ElephantLog/ElephantLog/Services/LogService.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using System.Text;
 using ElephantLog.Domain;
+using Microsoft.Extensions.Logging;
 
 namespace ElephantLog.Services
 {
     public class LogService : ILogService
     {
+        public ILogger<LogService> Log { get; }
+
+        public LogService(ILogger<LogService> log)
+        {
+            Log = log;
+        }
+
         public void LogMessage(LogEvent logEvent)
         {
-            throw new NotImplementedException();
+            Log.LogInformation(logEvent.ToString());
         }
     }
 }
