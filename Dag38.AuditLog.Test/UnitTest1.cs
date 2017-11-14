31ee5e3b22ed6faf365f0036b6dc0c61200c9f65
Hugo van Rijswijk <hugo.v.rijswijk@gmail.com>
2017-11-14 13:38:46 +0100

    Start of project

---
+++ Dag38.AuditLog.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dag38.AuditLog.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Dag38.AuditLog/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dag38.AuditLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
