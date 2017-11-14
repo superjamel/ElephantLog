9f1c6e0a6f60be339b4835b1e6e0f4620b7e3e2b
Hugo van Rijswijk <hugo.v.rijswijk@gmail.com>
2017-11-14 14:49:30 +0100

    Add controller to save logs

---
+++ ElephantLog/ElephantLog/Controllers/LogController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ElephantLog.Domain;
+using ElephantLog.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElephantLog.Controllers
+{
+    [Route("log")]
+    public class LogController : Controller
+    {
+        public ILogService LogService { get; }
+
+        public LogController(ILogService logService)
+        {
+            LogService = logService;
+        }
+
+        [HttpPost]
+        public IActionResult LogMessage([FromBody] LogEvent logEvent)
+        {
+            LogService.LogMessage(logEvent);
+            return Ok();
+        }
+    }
+}
