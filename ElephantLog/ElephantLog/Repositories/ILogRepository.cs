119b14988023418a74939aa7a8dead6479f1749a
Hugo van Rijswijk <hugo.v.rijswijk@gmail.com>
2017-11-14 16:42:56 +0100

    Add repository classes

---
+++ ElephantLog/ElephantLog/Repositories/ILogRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ElephantLog.Domain;
+
+namespace ElephantLog.Repositories
+{
+    public interface ILogRepository
+    {
+        void Save(LogEvent logEvent);
+    }
+}
---
+++ ElephantLog/ElephantLog/Repositories/LogRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ElephantLog.Domain;
+using MongoDB.Driver;
+
+namespace ElephantLog.Repositories
+{
+    public class LogRepository : ILogRepository
+    {
+        public IMongoDatabase Database { get; }
+        public IMongoCollection<LogEvent> Collection { get; }
+
+        public LogRepository(IMongoDatabase database)
+        {
+            Database = database;
+            Collection = Database.GetCollection<LogEvent>("LogEvents");
+        }
+        public void Save(LogEvent logEvent)
+        {
+            Collection.InsertOne(logEvent);
+        }
+    }
+}
