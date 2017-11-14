65f885711ca819a7e0fdeab0d130548aa1301f3e
Hugo van Rijswijk <hugo.v.rijswijk@gmail.com>
2017-11-14 14:22:08 +0100

    Add hello world controller

---
+++ ElephantLog/ElephantLog/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace ElephantLog.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public string HelloWorld()
+        {
+            return "Hello world";
+        }
+    }
+}
