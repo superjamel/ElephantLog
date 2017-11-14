a917ebc7b9ab390f85ac2d81bdfd8f835e74c3a1
Jamel de la Fuente <jamel.fuente@gmail.com>
2017-11-14 15:02:32 +0100

    Added MongoDB connection and create a collection.

--- ElephantLog/ElephantLog/Program.cs
+++ ElephantLog/ElephantLog/Program.cs
@@ -1,6 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace ElephantLog
 {
@@ -23,9 +31,25 @@ namespace ElephantLog
 
         public static IWebHost BuildWebHost(string[] args)
         {
-            return WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+            var webHost = new WebHostBuilder()
+               .UseKestrel()
+               .UseContentRoot(Directory.GetCurrentDirectory())
+               .ConfigureAppConfiguration((hostingContext, config) =>
+               {
+                   var env = hostingContext.HostingEnvironment;
+                   config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                         .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                   config.AddEnvironmentVariables();
+               })
+               .UseStartup<Startup>()
+               .UseSerilog(new LoggerConfiguration()
+                            .MinimumLevel.Verbose()
+                            .Enrich.FromLogContext()
+                            .WriteTo.Console(LogEventLevel.Information)
+                            .WriteTo.MongoDB("mongodb://localhost/logs", collectionName: "appAudits")
+                            .CreateLogger())
+                            .Build();
+            return webHost;
         }
     }
 }
