81cc4621ee3687a6cf59095440f8bfd2bf8331cb
Hugo van Rijswijk <hugo.v.rijswijk@gmail.com>
2017-11-16 15:53:45 +0100

    Add logging to logcontroller

--- ElephantLog/ElephantLog/Controllers/LogController.cs
+++ ElephantLog/ElephantLog/Controllers/LogController.cs
@@ -5,21 +5,25 @@ using ElephantLog.Domain;
 using ElephantLog.Services;
 using Microsoft.AspNetCore.Mvc;
 using Dag37.MQ.Lib;
+using Microsoft.Extensions.Logging;
 
 namespace ElephantLog.Controllers
 {
     public class LogController
     {
         public ILogService LogService { get; }
+        public ILogger<LogController> Log { get; }
 
-        public LogController(ILogService logService)
+        public LogController(ILogService logService, ILogger<LogController> log)
         {
             LogService = logService;
+            Log = log;
         }
 
         [Event("Events", "#")]
         public void LogMessage(LogEvent logEvent)
         {
+            Log.LogInformation("Received event {0} from {1}", logEvent.Body, logEvent.Server ?? "null");
             if (logEvent.TimeLogged == null)
             {
                 logEvent.TimeLogged = DateTime.Now;
